ce2b7f096267fc1f22a3c8a9b7c4f3cdff65da75
a123456789 <553640342@qq.com>
2019-12-01 19:25:20 +0800

    test

---
+++ src/main/java/org/hmf/rs/RightslipApplication.java
@@ -0,0 +1,22 @@
+package org.hmf.rs;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableTransactionManagement
+public class RightslipApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RightslipApplication.class, args);
+	}
+}
---
+++ src/main/resources/static/Component/Component.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>这是一个外部组件</h2>
+    </div>
+</template>
+
+<script type="module">
+    export default {
+    }
+</script>
+
+<style>
+
+</style>
